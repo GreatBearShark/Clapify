6ff399a0adfa6e1b9110ee887dca885332dc6478
GreatBearShark <m.wood0904@gmail.com>
2017-05-02 22:52:25 -0500

    Init

---
+++ app.js
@@ -0,0 +1,10 @@
+var form = document.getElementById('form');
+var inputClapify = document.getElementById("inputClapify");
+var outputClapify = document.getElementById("outputClapify");
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var output = inputClapify.value.replace(/ /g , ' \&#128079 ');
+  outputClapify.value = output;
+}
+);
