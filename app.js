6191418e0df355dcb4798bbe49fc29fca37327d7
GreatBearShark <m.wood0904@gmail.com>
2017-08-15 22:08:32 -0500

    Added two more dumb things

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var form = document.getElementById('form');
+var clapifyForm = document.getElementById('form');
 var inputClapify = document.getElementById("inputClapify");
 var outputClapify = document.getElementById("outputClapify");
 
-form.addEventListener('submit', (e) => {
+var waveForm = document.getElementById('wave-form');
+var inputWave = document.getElementById("inputWave");
+var outputWave = document.getElementById("outputWave");
+
+var bForm = document.getElementById('b-form');
+var inputB = document.getElementById("inputB");
+var outputB = document.getElementById("outputB");
+
+clapifyForm.addEventListener('submit', (e) => {
   e.preventDefault();
   var output = inputClapify.value.replace(/ /g , ' \&#128079 ');
   outputClapify.innerHTML = output;
-}
-);
+});
+
+waveForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	var output = inputWave.value.split('').join(' ');
+	outputWave.innerHTML = output;
+});
+
+bForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	var output = inputClapify.value.replace('b', '🅱');
+	outputB.innerHTML = output;
+});
+
